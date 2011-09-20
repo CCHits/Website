2d6eaba1d098f681c6675d62c4afcb66847b0d32
Jon "The Nice Guy" Spriggs <jon@sprig.gs>
2011-09-20 23:56:15 +0100

    Added a docblock to the logged_in.php file.

--- logged_in.php
+++ logged_in.php
@@ -1,3 +1,20 @@
+<?php
+/**
+ * CCHits.net is a website designed to promote Creative Commons Music,
+ * the artists who produce it and anyone or anywhere that plays it.
+ * These files are used to generate the site.
+ *
+ * PHP version 5
+ *
+ * @category Default
+ * @package  CCHitsClass
+ * @author   Jon Spriggs <jon@sprig.gs>
+ * @license  http://www.gnu.org/licenses/agpl.html AGPLv3
+ * @link     http://cchits.net Actual web service
+ * @link     http://code.cchits.net Developers Web Site
+ * @link     http://gitorious.net/cchits-net Version Control Service
+ */
+?>
 <pre>
 <?php
 session_start();
