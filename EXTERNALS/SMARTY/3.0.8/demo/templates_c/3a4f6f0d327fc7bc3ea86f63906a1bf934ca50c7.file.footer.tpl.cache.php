263b915530ea7dd20fbf453606301cd8474a2a72
Jon Spriggs <jon@sprig.gs>
2017-12-12 02:45:14 -0800

    Added externals. Note this is not a canonical source for these files. For those, please get them from the sources location.

---
+++ EXTERNALS/SMARTY/3.0.8/demo/templates_c/3a4f6f0d327fc7bc3ea86f63906a1bf934ca50c7.file.footer.tpl.cache.php
@@ -0,0 +1,21 @@
+<?php /* Smarty version Smarty-3.0.8, created on 2012-03-29 08:39:21
+         compiled from "./templates/footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:17042489294f748229933263-29665231%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '3a4f6f0d327fc7bc3ea86f63906a1bf934ca50c7' => 
+    array (
+      0 => './templates/footer.tpl',
+      1 => 1328473483,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '17042489294f748229933263-29665231',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+</BODY>
+</HTML>
---
+++ EXTERNALS/SMARTY/3.0.8/demo/templates_c/97c13ae6868bbc459509c9f1b968154acd23eecc.file.header.tpl.cache.php
@@ -0,0 +1,26 @@
+<?php /* Smarty version Smarty-3.0.8, created on 2012-03-29 08:39:21
+         compiled from "./templates/header.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:14131789544f7482297bac25-85311305%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '97c13ae6868bbc459509c9f1b968154acd23eecc' => 
+    array (
+      0 => './templates/header.tpl',
+      1 => 1328473482,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '14131789544f7482297bac25-85311305',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => true,
+)); /*/%%SmartyHeaderCode%%*/?>
+<HTML>
+<HEAD>
+<TITLE><?php echo $_smarty_tpl->getVariable('title')->value;?>
+ - <?php echo '/*%%SmartyNocache:14131789544f7482297bac25-85311305%%*/<?php echo $_smarty_tpl->getVariable(\'Name\')->value;?>
+/*/%%SmartyNocache:14131789544f7482297bac25-85311305%%*/';?></TITLE>
+</HEAD>
+<BODY bgcolor="#ffffff">
