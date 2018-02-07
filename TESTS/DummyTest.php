f618d7a565d954e937fd66574b95f2690f342b49
Yannick Mauray <yannick.mauray@gmail.com>
2018-02-07 13:51:45 +0100

    Added dummy test to check if the system is OK

---
+++ TESTS/DummyTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * CCHits.net is a website designed to promote Creative Commons Music,
+ * the artists who produce it and anyone or anywhere that plays it.
+ * These files are used to generate the site.
+ *
+ * PHP version 5
+ *
+ * @category Default
+ * @package  Tests
+ * @author   Yannick Mauray <yannick.mauray@gmail.com>
+ * @license  http://www.gnu.org/licenses/agpl.html AGPLv3
+ * @link     http://cchits.net Actual web service
+ * @link     https://github.com/CCHits/Website/wiki Developers Web Site
+ * @link     https://github.com/CCHits/Website Version Control Service
+ */
+use PHPUnit\Framework\TestCase;
+
+/**
+ * This class is a dummy unit test to check if the system works.
+ *
+ * @category Default
+ * @package  Tests
+ * @author   Yannick Mauray <yannick.mauray@gmail.com>
+ * @license  http://www.gnu.org/licenses/agpl.html AGPLv3
+ * @link     http://cchits.net Actual web service
+ * @link     https://github.com/CCHits/Website/wiki Developers Web Site
+ * @link     https://github.com/CCHits/Website Version Control Service
+ */
+
+final class DummyTest extends TestCase
+{
+    /**
+     * Dummy test.
+     * 
+     * @return void
+     */
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
